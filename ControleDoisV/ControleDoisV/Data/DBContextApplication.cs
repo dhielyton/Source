521303c0b8ff31e1c54f3c1f53d76d1640f9bc62
dhielyton lopes <dhielyton@hotmail.com>
2018-06-10 16:43:29 -0300

    Integração com as permissões de usuário;

---
+++ ControleDoisV/ControleDoisV/Data/DBContextApplication.cs
@@ -0,0 +1,19 @@
+﻿using ControleDoisV.Models;
+using DAL.Context;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDoisV.Data
+{
+    public class DBContextApplication : IdentityDbContext<UserApplication>
+    {
+        public DBContextApplication(DbContextOptions<DBContextApplication> options) : base(options)
+        {
+
+        }
+    }
+}
--- ControleDoisV/ControleDoisV/Startup.cs
+++ ControleDoisV/ControleDoisV/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControleDoisV.Data;
+using ControleDoisV.Models;
 using DAL.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,7 +28,19 @@ namespace ControleDoisV
         {
              services.AddDbContext<C2VContext>(options => options.UseSqlServer
              (Configuration.GetConnectionString("C2VConnection")));
+             services.AddDbContext<DBContextApplication>(options => options.UseSqlServer
+             (Configuration.GetConnectionString("C2VConnection")));
+          
             services.AddMvc();
+
+            services.AddIdentity<UserApplication, IdentityRole>()
+             .AddEntityFrameworkStores<DBContextApplication>()
+             .AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Infra/Acessar";
+                options.AccessDeniedPath = "/Infra/AcessoNegado";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

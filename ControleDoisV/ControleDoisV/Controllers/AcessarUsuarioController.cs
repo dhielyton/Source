97eef34a6380a7c355dcf4169b3ed1e590fac27f
dhielyton lopes <dhielyton@hotmail.com>
2018-06-10 17:27:34 -0300

    implementando o controller de acesso.

---
+++ ControleDoisV/ControleDoisV/Controllers/AcessarUsuarioController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleDoisV.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ControleDoisV.Controllers
+{
+    public class AcessarUsuarioController : Controller
+    {
+        private readonly UserManager<UserApplication> _userManager;
+        private readonly SignInManager<UserApplication> _signInManager;
+        private readonly ILogger _logger;
+
+        public AcessarUsuarioController(UserManager<UserApplication> userManager
+            , SignInManager<UserApplication> signInManager
+            , ILogger logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Acessar(string returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Acessar(AcessoUsuarioViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Senha, model.LembrarDeMim, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Usuário Autenticado");
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Fallha na tentativa de Login");
+            return View(model);
+        }
+
+        private void AddErros(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController), "Home");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Sair()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("Usuario realizou logout");
+            return RedirectToAction(nameof(HomeController), "Index");
+        }
+    }
+}
---
+++ ControleDoisV/ControleDoisV/Models/AcessoUsuarioViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDoisV.Models
+{
+    public class AcessoUsuarioViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Display(Name = "Lembrar de Mim")]
+        public bool LembrarDeMim { get; set; }
+    }
+}

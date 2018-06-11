2eedccabcc1519f3d41079309c7765c8dc65b06c
dhielyton lopes <dhielyton@hotmail.com>
2018-06-11 00:03:37 -0300

    refatoração e ajuste

--- ControleDoisV/Dominio/Entities/GrupoBem.cs
+++ ControleDoisV/Dominio/Entities/GrupoBem.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Dominio.Entities
 {
     public class GrupoBem:Entity
     {
+        [Display(Name ="ID")]
         public long GrupoBemID { get; set; }
 
+        [Display(Name = "Descricao")]
+        [Required]
         public string Descricao { get; set; }
     }
 }

c850a767c4eed0bccae3d964004c4d79e80741ad
dhielyton lopes <dhielyton@hotmail.com>
2018-06-11 00:47:50 -0300

    ajustes tabela de pessoa

--- ControleDoisV/Dominio/Entities/Pessoa.cs
+++ ControleDoisV/Dominio/Entities/Pessoa.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Dominio.Entities
 {
     public class Pessoa:Entity
     {
+        [Display(Name ="ID")]
         public int PessoaID { get; set; }
-
+        [Required]
         public string Nome { get; set; }
 
+        [Display(Name ="Observação")]
         public string Observacao { get; set; }
     }
 }
